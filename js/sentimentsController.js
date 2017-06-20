3c73e56448fc1e51bff4ce4b3a1c38a792ebf68a
matthew brozen <matthewbrozen@gmail.com>
2017-06-20 11:32:40 -0700

    initial commit

---
+++ js/sentimentsController.js
@@ -0,0 +1,43 @@
+angular.module('AylienApp')
+    .controller('SentimentsController', SentimentsController);
+
+//injecting http
+SentimentsController.$inject = ['$http'];
+// main function
+function SentimentsController($http) {
+  var self = this;
+   self.all=[];
+   self.newSentiment={};
+   self.addSentiment= addSentiment;
+
+   $http.get("https://aylienapi.herokuapp.com/sentiments")
+   .then(function(response) {
+     self.all = response.data.allSentiments;
+     console.log(self.all);
+   }, function(error) {
+     console.log(error)
+   });
+
+   //post report method need to store _id from response
+   function addSentiment(){
+    $http
+      .post('https://aylienapi.herokuapp.com/sentiments', self.newSentiment, console.log(self.newSentiment))
+      .then(function(response){
+        self.all.push(response.data.sentiment);
+        self.newSentiment = {};
+          $http.get('https://aylienapi.herokuapp.com/sentiments')
+          .then(function(response){
+            self.all = response.data.allSentiments;
+          }, function(error){
+            console.log(error)
+            return;
+          });
+
+    }, function(error){
+
+      console.log(error)
+      return;
+    });
+   }
+
+};
