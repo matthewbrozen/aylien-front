3d92b234b430ee7eef53459b723c5dcd483893b7
matthew brozen <matthewbrozen@gmail.com>
2017-06-21 15:45:20 -0700

    built out UI/UX

--- js/sentimentsController.js
+++ js/sentimentsController.js
@@ -2,19 +2,21 @@ angular.module('AylienApp')
     .controller('SentimentsController', SentimentsController);
 
 //injecting http
-SentimentsController.$inject = ['$http'];
+SentimentsController.$inject = ['$http', '$scope'];
 // main function
-function SentimentsController($http) {
+function SentimentsController($http, $scope) {
   var self = this;
    self.all=[];
+   self.positives=[];
+   self.neutral=[];
+   self.negative=[];
    self.newSentiment={};
    self.addSentiment= addSentiment;
    self.deleteSentiment = deleteSentiment;
 
-   $http.get("https://aylienapi.herokuapp.com/sentiments")
+   $http.get("http://localhost:3000/sentiments")
    .then(function(response) {
      self.all = response.data.allSentiments;
-     console.log(self.all);
    }, function(error) {
      console.log(error)
    });
@@ -22,11 +24,11 @@ function SentimentsController($http) {
    //post report method need to store _id from response
    function addSentiment(){
     $http
-      .post('https://aylienapi.herokuapp.com/sentiments', self.newSentiment, console.log(self.newSentiment))
+      .post('http://localhost:3000/sentiments', self.newSentiment, console.log(self.newSentiment))
       .then(function(response){
         self.all.push(response.data.sentiment);
         self.newSentiment = {};
-          $http.get('https://aylienapi.herokuapp.com/sentiments')
+          $http.get('http://localhost:3000/sentiments')
           .then(function(response){
             self.all = response.data.allSentiments;
           }, function(error){
