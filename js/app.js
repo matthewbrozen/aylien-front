9f52ba4445debd5545186a42a6ee0b7d9580742c
matthew brozen <matthewbrozen@gmail.com>
2017-06-23 17:55:11 -0700

    added animated spinner

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 angular.module('AylienApp', ["chart.js", "angular-loading-bar"])
   .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
     cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
-    cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner"><h4><em>Gathering Sentiments</h4></div>';
+    cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner"><h4><em>Gathering Sentiments<br><img src="default.gif"></h4></div>';
   }]);
