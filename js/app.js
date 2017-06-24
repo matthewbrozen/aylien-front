16f3d2f366cb833fb791c7637941a52dc880e0b1
matthew brozen <matthewbrozen@gmail.com>
2017-06-23 17:50:05 -0700

    order reversed and loading message

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-angular.module('AylienApp', ["chart.js", "angular-loading-bar"]);
+angular.module('AylienApp', ["chart.js", "angular-loading-bar"])
+  .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
+    cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner"><h4><em>Gathering Sentiments</h4></div>';
+  }]);
